786a60890d3d487362cbf93977ae89ecc2eeb3cd
miguelmitrus <miguelpmitrus@gmail.com>
2026-03-19 22:32:11 -0300

    Add files via upload

---
+++ numpar.py
@@ -0,0 +1,7 @@
+lista=[1,2,3,4,5,6,7 ]
+
+for n in lista:
+    if n%2==0:
+        numerospares+=1
+        
+        print(numerospares)
---
+++ somadnum.py
@@ -0,0 +1,6 @@
+lista=[5, 8, 12, 20 ]
+soma=0
+for n1 in lista:
+    soma+=n1
+
+print(soma)
